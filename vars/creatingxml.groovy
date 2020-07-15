436e989a2bf711e8ab31c72cc9aa7c5995795421
vamshibeju <vamshi.beju@gmail.com>
2020-07-16 00:55:26 +0530

    Update creatingxml.groovy

--- vars/creatingxml.groovy
+++ vars/creatingxml.groovy
@@ -3,13 +3,13 @@ def call(){
 def fileWriter = new FileWriter("/var/lib/jenkins/workspace/creatingxmljob/test.xml");
 
 def peopleBuilder = new groovy.xml.MarkupBuilder(fileWriter)
-def peopleBuilder1= "peopleBuilder.people {
+      peopleBuilder.people {
                   person {
                	 firstName('John')
                	 lastName('Doe')
                	 age(25)
-           		 }"
-        
+           		 }
+      }        
         fileWriter.close();
-}
+
 }
