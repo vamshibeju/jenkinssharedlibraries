afa9cd09bb4a9d8f6fea64416f11118cfe75089e
vamshibeju <vamshi.beju@gmail.com>
2020-07-26 19:29:09 +0530

    Create creatingxml.py

---
+++ resources/creatingxml.py
@@ -0,0 +1,12 @@
+
+document = """\
+<?xml version=“1.0” standalone=“yes” ?>
+<message>
+<from> Robert </from>
+<to> Mario </to>
+</message>
+"""
+file=open("test1.xml",'w')
+file.write(document)
+
+file.close()
