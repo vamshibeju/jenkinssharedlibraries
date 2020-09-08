9b8465d561ec48eb8dc377b259db88fb1b8a72d4
vamshibeju <vamshi.beju@gmail.com>
2020-09-08 13:53:09 +0530

    Update validatemaven.groovy

--- vars/validatemaven.groovy
+++ vars/validatemaven.groovy
@@ -3,12 +3,12 @@ def currPomversion = "${pom.version}"
 def fileExt = findFiles(glob: '**/**')
 for (int i=o; i<filesExt.size(); i++) {
       if("${fileExt[i].name}".contains("pom.xml"){
-      def readPom = readFile file: "${fileExt[i]"
-      def lines = readPom.readLines()
-      def lineNo = 0
-      for (line in lines){
-          lineNo ++
-          if(line.comtains("<version>${pomversion}</version>")){
-            sh script: "sed -i 's/$currPomversion}/${Relnumber}/g' ${filesExt[i]}"
-          }
-        }  
+            def readPom = readFile file: "${fileExt[i]"
+            def lines = readPom.readLines()
+            def lineNo = 0
+            for (line in lines){
+                lineNo ++
+                if(line.comtains("<version>${pomversion}</version>")){
+                  sh script: "sed -i 's/$currPomversion}/${Relnumber}/g' ${filesExt[i]}"
+                }
+              }  
