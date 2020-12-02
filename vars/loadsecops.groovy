1d8cea05ad3f2ce3da6b15e1ea2a094ada1e7539
vamshibeju <vamshi.beju@gmail.com>
2020-12-02 18:39:49 +0530

    Update loadsecops.groovy

--- vars/loadsecops.groovy
+++ vars/loadsecops.groovy
@@ -1,11 +1,12 @@
 def call(propname){
    try{
       echo "loadsecops groovy execution"
-      def toolsJsoncontent = libraryResource "globaltools.json"
-      if (toolsJsoncontent == null)
+      def toolsJsonContent = libraryResource "globaltools.json"
+      echo "json file loaded"
+      if (toolsJsonContent == null)
            error("unable to read json file")
       def splitvalue= propname.split("\\.")
-       toolsprop = readJSON text: toolsJsoncontent
+      toolsprop = readJSON text: toolsJsonContent
       echo "${splitvalue[0]}"
       return toolsprop."${splitvalue[0]}"."${splitvalue[1]}"."${splitvalue[2]}"
    
